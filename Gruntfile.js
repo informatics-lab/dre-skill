76b39adf764d27fabc11314ca968c61af0ab8fce
Niall Robinson <niall.h.robinson@gmail.com>
2016-01-20 12:03:46 +0000

    grunt file now checks to see if its on travis and get credentials fom ENVs if so

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,24 @@
 var grunt = require('grunt');
+var fs = require('fs');
 grunt.loadNpmTasks('grunt-aws-lambda');
 grunt.loadNpmTasks('grunt-shell');
+
+if (fs.existsSync(process.env.HOME+"/.aws/credentials")) {
+    theseOptions = {profile: "dre"};
+    console.log("using AWS credentials file")
+} else {
+    // if credentials file doesn't exists (presumes using environmental variables
+    theseOptions = {region: "us-east-1",
+                    accessKeyId: "$AWS_ACCESS_KEY_ID",
+                    secretAccessKey: "$AWS_SECRET_ACCESS_KEY"};
+    console.log("using AWS environmental variables")
+}
 
 grunt.initConfig({
     lambda_deploy: {
         default: {
             arn: 'arn:aws:lambda:us-east-1:536099501702:function:moDRE',
-        options: {
-                profile: "dre"
-            }
+        options: theseOptions
         }
     },
     lambda_package: {
