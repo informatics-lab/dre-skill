ead86432a5703bf5730e6918d060d039df275575
Rachel Prudden <rachel.prudden@informaticslab.co.uk>
2016-01-25 16:39:35 +0000

    added an activities config

---
+++ activities_config.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append("../dre")
+
+from actions import GaussDistFromIdeal
+
+class Condition(object):
+    def __init__(self, variable, ideal, min, max):
+        """ where variable name is correct to reference data base """
+        self.variable = variable
+        self.ideal = ideal
+        self.min = min
+        self.max = max
+
+activities_map = {
+	'run': ['run', 'jog', 'running', 'jogging'],
+	'sunbathe': ['sunbathe', 'sunbathing']
+}
+activities = dict((i, k) for k in activities_map for i in activities_map[k])
+
+run = {"score": GaussDistFromIdeal,
+       "conditions": [Condition("temperature", 15, 10, 23), Condition("precipitation", 0, 0, 50)],
+        "total_time": 3600,
+        "filter": []}
+
+sunbathe = {"score": GaussDistFromIdeal,
+            "conditions": [Condition("temperature", 28, 25, 40), Condition("precipitation", 0, 0, 0)],
+            "total_time": 7200,
+            "filter": []}
+
+def get_config(action_name, userID=None):
+	return __dict__[activities[action_name]]
--- config/activities_map.py
+++
@@ -1,6 +0,0 @@
-activities_map = {
-	'run': ['run', 'jog', 'running', 'jogging'],
-	'sunbathe': ['sunbathe', 'sunbathing']
-}
-
-activities = dict((i, k) for k in activities_map for i in activities_map[k])
