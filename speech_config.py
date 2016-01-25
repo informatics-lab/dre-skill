e4667f9683667981d4a42b83525feb368fcc0d60
Rachel Prudden <rachel.prudden@informaticslab.co.uk>
2016-01-25 16:40:36 +0000

    added speech config file

---
+++ speech_config.py
@@ -0,0 +1,14 @@
+from dotmap import DotMap
+
+session = DotMap({"greeting": "Hi. Welcome to the Met Office NLP app",
+		          "reprompt": "Hey, I said HI",
+		 		  "sign_off": "Thanks very much for using our app. See you later.",
+		   		  "help": "I can't help you."})
+
+Time = DotMap({"question": "When do you want to do that",
+		       "reprompt": "I SAID WHEN DO YOU WANT TO DO THAT!",
+		       "help": "Which word don't you understand"})
+
+Location = DotMap({"question": "Where do you want to do that",
+		           "reprompt": "I SAID WHERE DO YOU WANT TO DO THAT!",
+		           "help": "Which word don't you understand"})
