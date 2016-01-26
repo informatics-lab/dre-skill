b4e70227df2a96ffb44ee5803ce6fafc818dc79d
Rachel Prudden <rachel.prudden@informaticslab.co.uk>
2016-01-26 11:29:46 +0000

    now passes tests

--- speech_config.py
+++ speech_config.py
@@ -4,14 +4,18 @@ session = DotMap({"greeting": "Hi. Welcome to the Met Office NLP app",
 		          "reprompt": "Hey, I said HI",
 		 		  "sign_off": "Thanks very much for using our app. See you later.",
 		   		  "help": "I can't help you."})
-Activity = DotMap({"question": "What do you want to do",
-		       "reprompt": "I SAID WHAT DO YOU WANT TO DO!",
-		       "help": "Which word don't you understand"})
 
-Time = DotMap({"question": "When do you want to do that",
-		       "reprompt": "I SAID WHEN DO YOU WANT TO DO THAT!",
-		       "help": "Which word don't you understand"})
+Activity = DotMap({"title": "TITLE",
+				   "question": "What do you want to do",
+		           "reprompt": "I SAID WHAT DO YOU WANT TO DO!",
+		           "help": "Which word don't you understand"})
 
-Location = DotMap({"question": "Where do you want to do that",
+start_time = DotMap({"title": "TITLE",
+				     "question": "When do you want to do that",
+		             "reprompt": "I SAID WHEN DO YOU WANT TO DO THAT!",
+		             "help": "Which word don't you understand"})
+
+location = DotMap({"title": "TITLE",
+				   "question": "Where do you want to do that",
 		           "reprompt": "I SAID WHERE DO YOU WANT TO DO THAT!",
 		           "help": "Which word don't you understand"})
