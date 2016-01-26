ab029648f45d30308177ada8e3b7fc9a58e189df
Niall Robinson <niall.h.robinson@gmail.com>
2016-01-26 12:05:28 +0000

    changed to camel case

--- speech_config.py
+++ speech_config.py
@@ -5,12 +5,12 @@ session = DotMap({"greeting": "Hi. Welcome to the Met Office NLP app",
 		 		  "sign_off": "Thanks very much for using our app. See you later.",
 		   		  "help": "I can't help you."})
 
-Activity = DotMap({"title": "TITLE",
+activity = DotMap({"title": "TITLE",
 				   "question": "What do you want to do",
 		           "reprompt": "I SAID WHAT DO YOU WANT TO DO!",
 		           "help": "Which word don't you understand"})
 
-start_time = DotMap({"title": "TITLE",
+startTime = DotMap({"title": "TITLE",
 				     "question": "When do you want to do that",
 		             "reprompt": "I SAID WHEN DO YOU WANT TO DO THAT!",
 		             "help": "Which word don't you understand"})
