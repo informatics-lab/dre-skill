91df37da06c540f8bc5b3ea8e4d74a2d9a45b151
Niall Robinson <niall.h.robinson@gmail.com>
2016-01-27 09:55:03 +0000

    moved inputs and outputs for tests

--- tests/lambda_tests.py
+++ tests/lambda_tests.py
@@ -2,6 +2,7 @@ import yaml
 import unittest
 import pickle
 
+import os
 import sys
 sys.path.append("..")
 
@@ -9,12 +10,11 @@ from intent_processing.lambda_fn import *
 
 class LambdaDecisionTest(unittest.TestCase):
     base = os.path.split(__file__)[0]
-
-    with open(os.path.join(base, 'sample_event.json'), 'r') as evtfile:
+    with open(os.path.join(base, 'json_packets', 'in', 'sample_event.json'), 'r') as evtfile:
         event = yaml.safe_load(evtfile.read())
 
     cache = ForecastCache()
-    with open("./tests/testForecast.pkl", "rb") as f:
+    with open(os.path.join(base, 'data', 'testForecast.pkl'), "rb") as f:
         timesteps = pickle.load(f)
     cache.cacheForecast(timesteps, Loc(lat=50.7, lon=-3.5))
 
@@ -22,6 +22,9 @@ class LambdaDecisionTest(unittest.TestCase):
         answer = 'Wher'
         result = go(self.event, None, self.cache)
         self.assertEquals(result['response']['outputSpeech']['text'][:4], answer)
+
+# class SessionPersistenceTest(unittest.TestCase):
+    
 
 if __name__ == '__main__':
     unittest.main()
