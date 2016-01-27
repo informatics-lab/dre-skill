176661c60bde5d90dbc09250e2644f3750ef864b
Niall Robinson <niall.h.robinson@gmail.com>
2016-01-27 12:11:02 +0000

    added more test jsons

--- tests/lambda_tests.py
+++ tests/lambda_tests.py
@@ -23,8 +23,31 @@ class LambdaDecisionTest(unittest.TestCase):
         result = go(self.event, None, self.cache)
         self.assertEquals(result['response']['outputSpeech']['text'][:4], answer)
 
-# class SessionPersistenceTest(unittest.TestCase):
-    
+
+class SessionPersistenceTest(unittest.TestCase):
+    base = os.path.split(__file__)[0]
+
+    cache = ForecastCache()
+    with open(os.path.join(base, 'data', 'testForecast.pkl'), "rb") as f:
+        timesteps = pickle.load(f)
+    cache.cacheForecast(timesteps, Loc(lat=50.7, lon=-3.5))
+
+    with open(os.path.join(base, 'json_packets', 'in', 'whenshalligoforarun.json'), 'r') as f:
+        initialInput = yaml.safe_load(f.read())
+    with open(os.path.join(base, 'json_packets', 'out', 'whenshalligoforarun.json'), 'r') as f:
+        initialOutput = yaml.safe_load(f.read())
+    with open(os.path.join(base, 'json_packets', 'in', 'inExeter.json'), 'r') as f:
+        secondaryInput = yaml.safe_load(f.read())
+    with open(os.path.join(base, 'json_packets', 'out', 'whenshalligoforarun_inexeter.json'), 'r') as f:
+        secondaryOutput = yaml.safe_load(f.read())
+
+    def testDialogueIntent(self):
+        """ Should ask for another slot """
+        thisInitialResult = go(self.initialInput, None, self.cache)
+        self.assertEquals(thisInitialResult, self.initialOutput)
+        thisSecondaryResult = go(thisInitialResult, None, self.cache)
+        self.assertEquals(thisSecondaryResult, self.secondaryOutput)
+
 
 if __name__ == '__main__':
     unittest.main()
