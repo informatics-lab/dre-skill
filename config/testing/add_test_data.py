14aa452a4e1495378f104a9bebc291b636f28b01
Niall Robinson <niall.h.robinson@gmail.com>
2016-02-09 12:05:46 +0000

    now effectively merged in master

--- config/testing/add_test_data.py
+++ config/testing/add_test_data.py
@@ -11,12 +11,15 @@ import os
 if __name__ == "__main__":
     client = MongoClient(os.getenv("MONGO_DB"))
 
-    with open("./activities_config.json", "r") as f:
+    base = os.path.split(__file__)[0]
+    with open(os.path.join(base, 'activities_config.json'), "r") as f:
         test_act_conf = json.loads(f.read())
-    with open("./speech_config.json", "r") as f:
+    with open(os.path.join(base, 'speech_config.json'), "r") as f:
         test_speech_conf = json.loads(f.read())
 
     print "Uploading test activity config"
-    client["dre"]["activities_configs"].replace_one({"_id": "tests"}, test_act_conf, upsert=True)
+    client["dre"]["activity_configs"].replace_one({"_id": "tests"}, test_act_conf, upsert=True)
+    print client["dre"]["activity_configs"].find_one(filter="tests")
     print "Uploading test speech config"
     client["dre"]["speech_configs"].replace_one({"_id": "tests"}, test_speech_conf, upsert=True)
+    print client["dre"]["speech_configs"].find_one(filter="tests")
