8ed14f4cb6f535c2a738ef71bc3a0d913e147847
Niall Robinson <niall.h.robinson@gmail.com>
2016-02-09 14:12:49 +0000

    generalised the config -> mongo upserter

--- config/testing/add_test_data.py
+++ config/testing/add_test_data.py
@@ -8,18 +8,23 @@ import json
 from pymongo import MongoClient
 import os
 
-if __name__ == "__main__":
+
+def upsert_configs(activities_conf_file, speech_config_file, uid):
     client = MongoClient(os.getenv("MONGO_DB"))
 
     base = os.path.split(__file__)[0]
-    with open(os.path.join(base, 'activities_config.json'), "r") as f:
+    with open(os.path.join(base, activities_conf_file), "r") as f:
         test_act_conf = json.loads(f.read())
-    with open(os.path.join(base, 'speech_config.json'), "r") as f:
+    with open(os.path.join(base, speech_config_file), "r") as f:
         test_speech_conf = json.loads(f.read())
 
     print "Uploading test activity config"
-    client["dre"]["activity_configs"].replace_one({"_id": "tests"}, test_act_conf, upsert=True)
-    print client["dre"]["activity_configs"].find_one(filter="tests")
+    client["dre"]["activity_configs"].replace_one({"_id": uid}, test_act_conf, upsert=True)
+    print client["dre"]["activity_configs"].find_one(filter=uid)
     print "Uploading test speech config"
-    client["dre"]["speech_configs"].replace_one({"_id": "tests"}, test_speech_conf, upsert=True)
-    print client["dre"]["speech_configs"].find_one(filter="tests")
+    client["dre"]["speech_configs"].replace_one({"_id": uid}, test_speech_conf, upsert=True)
+    print client["dre"]["speech_configs"].find_one(filter=uid)
+
+
+if __name__ == "__main__":
+    upsert_configs("activities_config.json", "speech_config.json", "tests")
