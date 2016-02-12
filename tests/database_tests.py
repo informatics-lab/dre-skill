44aa25ff93ab073ab01d3490573fc3fae718be7e
Niall Robinson <niall.h.robinson@gmail.com>
2016-02-12 16:19:45 +0000

    fixed the get table method to properly use boto3

--- tests/database_tests.py
+++ tests/database_tests.py
@@ -5,9 +5,10 @@ import unittest
 import os
 
 from database import database
+from intent_processing.intent_request_handlers import make_card
 
 
-class TestConfig(unittest.TestCase):
+class TestDatabase(unittest.TestCase):
     base = os.path.split(__file__)[0]
     with open(os.path.join(base, 'json_packets', 'in', 'activities_config.json'), 'r') as f:
         test_act_conf = json.loads(f.read())
@@ -20,6 +21,17 @@ class TestConfig(unittest.TestCase):
         self.assertTrue(all([type(condition) is database.Condition
                                 for values in parsed["activities"].values()
                                 for condition in values["conditions"]]))
+
+    def test_logs(self):
+        session_id = "sd39f90f"
+        user_id = "adf3333"
+        log = {"I'm a json!"}
+        make_card(session_id, user_id, log)
+
+        got_log = database.get_log(session_id)
+        assertEquals(got_log["log"], log)
+
+        database.remove_log(session_id)
 
 
 if __name__ == '__main__':
