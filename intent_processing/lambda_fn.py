236bc8916d63736da5a768685cc043162ddc187a
Rachel Prudden <rachel.prudden@informaticslab.co.uk>
2016-02-23 13:29:17 +0000

    slightly improved config handling
    Conflicts:
    	database/database.py
    	intent_processing/lambda_fn.py
    	tests/lambda_tests.py

--- intent_processing/lambda_fn.py
+++ intent_processing/lambda_fn.py
@@ -8,12 +8,10 @@ import conversation
 
 
 def go(event, context, speech_config_name="default", cache=ForecastCache()):
-    a_default_values = database.get_default_values_conf(event["session"]["user"]["userId"])
-    t_default_values = database.get_default_time_slot_values_conf(speech_config_name)
+    default_values = database.get_config(event["session"]["user"]["userId"])
     speech_config = database.get_speech_conf(speech_config_name)
-
     try:
-        session = conversation.Session(event, context, speech_config, a_default_values, t_default_values, 'activity', cache=cache)
+        session = conversation.Session(event, context, speech_config, default_values, 'activity', cache=cache)
         return session.respond()
     except conversation.PrimarySlotError as e:
         return e.message
